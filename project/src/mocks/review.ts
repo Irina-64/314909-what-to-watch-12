329945c78e6aab996aade0e6eb6d94a81d9082f0
Irina Trubchik <trubchik@mail.ru>
2023-05-16 04:25:21 +0300

    добавит тестовые данные в mocks

---
+++ project/src/mocks/review.ts
@@ -0,0 +1,18 @@
+import Review from '../types/review';
+
+const REVIEW_LIST: Review[] = [
+  {
+    reviewId: '1',
+    movieName: 'The Shawshank Redemption',
+    text: 'very interesting story',
+    islike: true,
+  },
+  {
+    reviewId: '2',
+    movieName: 'The Shawshank Redemption',
+    text: 'very awful story',
+    islike: false,
+  },
+];
+
+export default REVIEW_LIST;
